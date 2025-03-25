017d3c1d3c73a1c08a1f977aa26e5fc854a988c4
YuJieWang24 <YuJieWorkstation@gmail.com>
2025-03-26 00:13:43 +0800

    refactor(ts): 變更產品建構式

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Product } from "./product";
 
-let product = new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2025-3-19"), undefined);
+let product = new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2025-3-19"));
 
 console.log(product);
 console.table(product);
