02cc42c2bdf286461d78628bd2dbbd28d04daf4e
YuJieWang24 <YuJieWorkstation@gmail.com>
2025-04-03 23:24:29 +0800

    feat(product): 利用文字叉值顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,11 +1,14 @@
+import { Product } from './../../assets/code/product';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  productName = '書籍 A';
+  autor = '作者甲、作者乙、作者丙';
+  company = '柏碩文化';
 }
